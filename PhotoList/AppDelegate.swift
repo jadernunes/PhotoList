c4246c1c04e9ebcf80b55e2d9f446f8bbecd3d2a
Jader Nunes <jadernunes.jbn@gmail.com>
2021-01-25 08:14:16 -0300

    added xcode project

---
+++ PhotoList/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  PhotoList
+//
+//  Created by Jader Nunes on 2021-01-25.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        true
+    }
+}
---
+++ PhotoList/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  PhotoList
+//
+//  Created by Jader Nunes on 2021-01-25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
