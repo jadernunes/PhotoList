3225407e53202ca1f495acaef7d505251da90779
Jader Nunes <jadernunes.jbn@gmail.com>
2021-01-30 01:24:00 -0300

    List all device's photos

---
+++ PhotoList/Module/Home/ViewController/HomeViewController+Extension.swift
@@ -0,0 +1,22 @@
+//
+//  HomeViewController+Extension.swift
+//  PhotoList
+//
+//  Created by Jader Nunes on 2021-01-29.
+//
+
+// MARK: - Constraints
+
+import UIKit
+
+extension HomeViewController {
+    
+    func addCollectionView() {
+        view.addSubview(collectionView)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        collectionView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        collectionView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+    }
+}
---
+++ PhotoList/Module/Home/ViewController/HomeViewController.swift
@@ -0,0 +1,144 @@
+//
+//  HomeViewController.swift
+//  PhotoList
+//
+//  Created by Jader Nunes on 2021-01-25.
+//
+
+import UIKit
+
+private enum ItemsPerPage: Int {
+    case portrait = 3
+}
+
+final class HomeViewController: BaseViewController {
+    
+    // MARK: - Attributes
+    
+    private(set) var viewModel: HomeViewModelProtocol?
+    
+    // MARK: - Outlets
+    
+    private(set) lazy var collectionView: UICollectionView = {
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.minimumLineSpacing = 8
+        
+        let collectionView = UICollectionView(frame: .infinite, collectionViewLayout: flowLayout)
+        collectionView.alwaysBounceVertical = true
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.backgroundColor = .clear
+        collectionView.contentInset = .init(top: 16, left: 8, bottom: 16, right: 8)
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.showsVerticalScrollIndicator = false
+        collectionView.register(PhotoCell.self, forCellWithReuseIdentifier: PhotoCell.className)
+        return collectionView
+    }()
+    private(set) lazy var buttonAdd = UIBarButtonItem(barButtonSystemItem: .add,
+                                                      target: self,
+                                                      action: #selector(buttonAddPressed))
+    
+    // MARK: - Life cycle
+    
+    init(viewModel: HomeViewModelProtocol) {
+        super.init()
+        
+        self.viewModel = viewModel
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupUI()
+        createElements()
+        bindUI()
+        viewModel?.loadPhotos()
+    }
+    
+    // MARK: - Custom methods
+    
+    private func setupUI() {
+        view.backgroundColor = .customDarkGray()
+        title = .localizable("titleHome")
+        navigationItem.rightBarButtonItems = [buttonAdd]
+        configureNavigationBar(isHidden: false, prefersLargeTitles: true)
+    }
+    
+    private func bindUI() {
+        bindData()
+        bindLoader()
+    }
+    
+    private func createElements() {
+        addCollectionView()
+    }
+    
+    private func sizeGalley() -> CGSize {
+        switch UIDevice.current.orientation {
+        case .faceDown, .faceUp, .portrait, .portraitUpsideDown:
+            let dimension = view.frame.width / 3 - CGFloat((ItemsPerPage.portrait.rawValue * 4))
+            return .init(width: dimension, height: dimension)
+        default:
+            return .zero
+        }
+    }
+    
+    // MARK: - Actions
+    
+    @objc private func buttonAddPressed() {
+        //TODO: Take picture
+    }
+}
+
+// MARK: - Binds
+
+private extension HomeViewController {
+    
+    func bindData() {
+        viewModel?.dataDidChange.bind { [weak self] _ in
+            DispatchQueue.main.async { [weak self] in
+                self?.collectionView.reloadData()
+            }
+        }
+    }
+    
+    func bindLoader() {
+        viewModel?.isLoading.bind { [weak self] isLoading in
+            isLoading
+                ? self?.collectionView.startLoading()
+                : self?.collectionView.stopLoading()
+        }
+    }
+}
+
+// MARK: - CollectionView datasource
+
+extension HomeViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        viewModel?.countPhotos() ?? 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PhotoCell.className, for: indexPath) as? PhotoCell,
+            let data = viewModel?.photo(indexPath.row)
+        else { return UICollectionViewCell() }
+        
+        cell.configure(viewModel: PhotoCellViewModel(photo: data))
+        return cell
+    }
+}
+
+// MARK: - Collection view flow delegate
+
+extension HomeViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        sizeGalley()
+    }
+}
